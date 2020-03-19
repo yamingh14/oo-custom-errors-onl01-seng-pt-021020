a2b3a3c87e4602f5f8c4027226f827596a648173
Yamin Ghowrwal <yamin.gh@gmail.com>
2020-03-19 23:29:35 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -6,13 +6,17 @@ class Person
   end
 
   def get_married(person)
-   self.partner = person
-   if person.class != Person
-     raise PartnerError
-   else
-     person.partner = self
-   end
- end
+      self.partner = person
+      if person.class != Person
+        begin
+          raise PartnerError
+        rescue PartnerError => error
+            puts error.message
+        end
+      else
+        person.partner = self
+      end
+  end
 
 class PartnerError < StandardError
    def message
