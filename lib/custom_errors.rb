a1de51f8a6e60d638d97766495cb4a4690bd5922
Yamin Ghowrwal <yamin.gh@gmail.com>
2020-03-19 23:18:12 +0000

    Automatically backed up by Learn

---
+++ lib/custom_errors.rb
@@ -0,0 +1,16 @@
+class Person
+  attr_accessor :name, :partner
+ 
+  def initialize(name)
+    @name = name
+  end
+ 
+  def get_married(person)
+    self.partner = person
+    person.partner = self
+  end
+end
+ 
+beyonce = Person.new("Beyonce")
+beyonce.get_married("Jay-Z")
+puts beyonce.name
